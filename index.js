e1521e9431b1d7ceabbc112a3a2ee3b83a2f92b7
Diogo F. Guzenski <diogofraga132@hotmail.com>
2018-11-20 12:37:08 -0200

    0
    0

--- index.js
+++ index.js
@@ -5,41 +5,54 @@ const file = 'lista.txt'
 //const data = 'Lucas | 22    |Torres-RS\n'
 const encoding = 'utf8'
 
+prompt.start();
+
+
 function add(data) {
-    fs.writeFile(file, data, { enconding: 'utf-8', flag: 'a' }, function (err) {
+    fs.writeFileSync(file, data, { enconding: 'utf-8', flag: 'a' }, function (err) {
         if (err) throw err;
         console.log("Arquivo Salvo \n");
     });
 }
-var done = false;
-var resp = ""
-prompt.start();
 
 
 function adicionar() {
+    console.log('\n');
     prompt.get(['nome', 'cpf', 'endereco'], function (err, result) {
 
-        console.log('Command-line input received:');
-        console.log('  nome: ' + result.nome);
-        console.log('  cpf: ' + result.cpf);
-        console.log('endereco:' + result.endereco);
+        console.log('\nDados inseridos:');
+        console.log('   nome: ' + result.nome);
+        console.log('   cpf: ' + result.cpf);
+        console.log('   endereco:' + result.endereco);
 
         let nome = result.nome;
         let cpf = result.cpf;
         let endereco = result.endereco;
-        let id = 0;
-        id++;
-
+        let id =0;
         let data = "|" + (id) + "|" + (nome) + "|" + (cpf) + "|" + (endereco) + "| \n"
         add(data);
-    })
+
+        console.log('\n Deseja continuar? 1-para sim')
+        prompt.get(['resposta'], function (err, result) {
+            if (result.resposta == 1) {
+                adicionar();
+            }
+        });
+    });
 }
-/*prompt.get(['continuar'], function (err, result) {
-    resp = result.continuar
-    if (resp === 'sim') {
-        done = true;
-    }
-});
-while (done != true) {
-    add();
-}*/
+
+function pegarUltimoId() {
+    fs.readFileSync('lista.txt', function (err, data) {
+        if (err) {
+            console.error("Could not open file: %s", err);
+            process.exit(1);
+        }
+        //
+        var dados= data.toString('utf8');
+        console.log(dados);
+        //console.log(data.toString('utf8'));
+    });
+
+}
+adicionar();
+pegarUltimoId();
